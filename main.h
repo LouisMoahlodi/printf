4af9543cb0d0c6d762595589d728546c5ec54867
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-10-17 17:35:12 +0200

    Added on to Header file

--- main.h
+++ main.h
@@ -1,9 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdarg.h>
 
 /**
@@ -20,5 +19,10 @@ typedef struct print
 
 int _printf(const char *format, ...);
 int print_int(va_list arguments, char *buf, unsigned ibuf);
+
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int print_buf(char *buf, unsigned int nbuf);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+
 
 #endif /* MAIN_H */
