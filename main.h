a41210ecdff966f9a8e8def0bc0156a83b2b7eec
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-10-17 14:00:33 +0200

    Added the _printf.c file

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * struct print - struct stores pointers to a printer function
+ * 
+ * @type_arg: indentifier 
+ * @f: pointer to a print functions
+ */
+typedef struct print
+{
+    char *type_arg;
+    int (*f)(va_list, char *, unsigned);
+}print_t;
+
+int _printf(const char *format, ...);
